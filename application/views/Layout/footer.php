12d3503be2cf013c6c2ccd06f0b13f47442ab00a
ImamRenaldy <imam.renaldy26@gmail.com>
2024-11-28 15:03:55 +0700

    fixed

--- application/views/Layout/footer.php
+++ application/views/Layout/footer.php
@@ -1,14 +1,14 @@
 <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> 3.2.0
+  </div>
+  <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+</footer>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
   <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
@@ -43,7 +43,6 @@
 <script src="<?= base_url("assets/")?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?= base_url("assets/")?>dist/js/adminlte.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="<?= base_url("assets/")?>dist/js/demo.js"></script>
+
 </body>
 </html>
