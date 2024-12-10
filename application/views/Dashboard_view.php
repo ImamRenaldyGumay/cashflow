c26e1670b9f46b2b85d5fc5beef085b95a247fbf
ImamRenaldy <imam.renaldy26@gmail.com>
2024-12-10 08:41:37 +0700

    fixed

--- application/views/Dashboard_view.php
+++ application/views/Dashboard_view.php
@@ -1,125 +1,92 @@
-<!-- Main Content -->
-<div class="main-content">
-  <section class="section">
-    <div class="section-header">
-      <h1>DataTables</h1>
-      <div class="section-header-breadcrumb">
-        <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-        <div class="breadcrumb-item"><a href="#">Modules</a></div>
-        <div class="breadcrumb-item">DataTables</div>
-      </div>
-    </div>
 
-    <div class="section-body">
-      <h2 class="section-title">DataTables</h2>
-      <!-- <p class="section-lead">
-        We use 'DataTables' made by @SpryMedia. You can check the full documentation <a href="https://datatables.net/">here</a>.
-      </p> -->
-      <div class="row">
-        <div class="col-12">
-          <div class="card">
-            <div class="card-header d-flex justify-content-between">
-              <h4>Basic DataTables</h4>
-              <button class="btn btn-primary btn-lg">Tambah Data</button>
+
+        
+
+        
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1 class="m-0">Starter Page</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+        </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+    <div class="container-fluid">
+        <div class="row">
+        <div class="col-lg-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+
+                <p class="card-text">
+                Some quick example text to build on the card title and make up the bulk of the card's
+                content.
+                </p>
+
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+            </div>
+
+            <div class="card card-primary card-outline">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+
+                <p class="card-text">
+                Some quick example text to build on the card title and make up the bulk of the card's
+                content.
+                </p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+            </div><!-- /.card -->
+        </div>
+        <!-- /.col-md-6 -->
+        <div class="col-lg-6">
+            <div class="card">
+            <div class="card-header">
+                <h5 class="m-0">Featured</h5>
             </div>
             <div class="card-body">
-              <div class="table-responsive">
-                <table class="table table-striped" id="table-1">
-                  <thead>                                 
-                    <tr>
-                      <th class="text-center">
-                        #
-                      </th>
-                      <th>Task Name</th>
-                      <th>Progress</th>
-                      <th>Members</th>
-                      <th>Due Date</th>
-                      <th>Status</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>                                 
-                    <tr>
-                      <td>
-                        1
-                      </td>
-                      <td>Create a mobile app</td>
-                      <td class="align-middle">
-                        <div class="progress" data-height="4" data-toggle="tooltip" title="100%">
-                          <div class="progress-bar bg-success" data-width="100%"></div>
-                        </div>
-                      </td>
-                      <td>
-                        <img alt="image" src="assets/img/avatar/avatar-5.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Wildan Ahdian">
-                      </td>
-                      <td>2018-01-20</td>
-                      <td><div class="badge badge-success">Completed</div></td>
-                      <td><a href="#" class="btn btn-secondary">Detail</a></td>
-                    </tr>
-                    <tr>
-                      <td>
-                        2
-                      </td>
-                      <td>Redesign homepage</td>
-                      <td class="align-middle">
-                        <div class="progress" data-height="4" data-toggle="tooltip" title="0%">
-                          <div class="progress-bar" data-width="0"></div>
-                        </div>
-                      </td>
-                      <td>
-                        <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Nur Alpiana">
-                        <img alt="image" src="assets/img/avatar/avatar-3.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Hariono Yusup">
-                        <img alt="image" src="assets/img/avatar/avatar-4.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Bagus Dwi Cahya">
-                      </td>
-                      <td>2018-04-10</td>
-                      <td><div class="badge badge-info">Todo</div></td>
-                      <td><a href="#" class="btn btn-secondary">Detail</a></td>
-                    </tr>
-                    <tr>
-                      <td>
-                        3
-                      </td>
-                      <td>Backup database</td>
-                      <td class="align-middle">
-                        <div class="progress" data-height="4" data-toggle="tooltip" title="70%">
-                          <div class="progress-bar bg-warning" data-width="70%"></div>
-                        </div>
-                      </td>
-                      <td>
-                        <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Rizal Fakhri">
-                        <img alt="image" src="assets/img/avatar/avatar-2.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Hasan Basri">
-                      </td>
-                      <td>2018-01-29</td>
-                      <td><div class="badge badge-warning">In Progress</div></td>
-                      <td><a href="#" class="btn btn-secondary">Detail</a></td>
-                    </tr>
-                    <tr>
-                      <td>
-                        4
-                      </td>
-                      <td>Input data</td>
-                      <td class="align-middle">
-                        <div class="progress" data-height="4" data-toggle="tooltip" title="100%">
-                          <div class="progress-bar bg-success" data-width="100%"></div>
-                        </div>
-                      </td>
-                      <td>
-                        <img alt="image" src="assets/img/avatar/avatar-2.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Rizal Fakhri">
-                        <img alt="image" src="assets/img/avatar/avatar-5.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Isnap Kiswandi">
-                        <img alt="image" src="assets/img/avatar/avatar-4.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Yudi Nawawi">
-                        <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle" width="35" data-toggle="tooltip" title="Khaerul Anwar">
-                      </td>
-                      <td>2018-01-16</td>
-                      <td><div class="badge badge-success">Completed</div></td>
-                      <td><a href="#" class="btn btn-secondary">Detail</a></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+                <h6 class="card-title">Special title treatment</h6>
+
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
-          </div>
+            </div>
+
+            <div class="card card-primary card-outline">
+            <div class="card-header">
+                <h5 class="m-0">Featured</h5>
+            </div>
+            <div class="card-body">
+                <h6 class="card-title">Special title treatment</h6>
+
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
         </div>
-      </div>
+        <!-- /.col-md-6 -->
+        </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
     </div>
-  </section>
+    <!-- /.content -->
 </div>
+<!-- /.content-wrapper -->
